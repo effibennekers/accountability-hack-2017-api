b1981ffce343aeb00a05d812824d43b2132796b9
Effi Bennekers <fraim.bennekers@ing.nl>
2017-06-09 09:17:46 +0200

    Init

---
+++ src/main/java/com/theaccountant/accountabilityhack/AccountabilityHackApplication.java
@@ -0,0 +1,12 @@
+package com.theaccountant.accountabilityhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountabilityHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountabilityHackApplication.class, args);
+	}
+}
