383c1668739c9ec3e47ad21ee0137465591e2401
qc53bf <jesse.brand@ing.nl>
2017-06-09 13:15:15 +0200

    Use builder to create object.

--- src/main/java/com/theaccountant/accountabilityhack/processor/FteAantalProcessor.java
+++ src/main/java/com/theaccountant/accountabilityhack/processor/FteAantalProcessor.java
@@ -15,7 +15,7 @@ public final class FteAantalProcessor implements Processor {
             if (registry.isSchoolPresent(brin)) {
                 entry = registry.getSchoolByBrin(brin);
             } else {
-                entry = new SchoolEntry(brin);
+                entry = SchoolEntry.builder().brin(brin).build();
                 registry.addSchool(entry);
             }
             final String functie = reader.get("FUNCTIEGROEP");
